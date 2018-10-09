b02bc8388a58d47c76d027bcf3ceca52ab8fd928
tuanweb <nguyenanhtuan92my@gmail.com>
2018-10-09 09:01:24 +0700

    Update App1/AppOne.py

--- App1/AppOne.py
+++ App1/AppOne.py
@@ -1,10 +1,16 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(w):
-    if w.lower() in data:
+    w = w.lower()
+    if w in data:
         return data[w]
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn = input(" Did you mean '%s'  instead ? Enter Y if yes , or N if no :  " % get_close_matches(w,data.keys())[0] )
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
     else:
         return "The word doesn't exist. Please double check it."
 
