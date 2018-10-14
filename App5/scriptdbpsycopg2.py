0f2c369981ca896ec2f4221f2a5b268a40875495
nguyenanhtuan92my@gmail.com <nguyenanhtuan92my@gmail.com>
2018-10-14 22:14:47 +0700

    nnn

--- App5/scriptdbpsycopg2.py
+++ App5/scriptdbpsycopg2.py
@@ -15,14 +15,14 @@ def insert(item,quantity,price):
     # cur.execute(" INSERT INTO store VALUES ('Wine Glass',8,10.5)")
     # cur.execute(" INSERT INTO store VALUES (?,?,?)",(item,quantity,price))
     # work well but injection SQLcur.execute(" INSERT INTO store VALUES ('%s', '%s' , '%s')" %(item,quantity,price))
-    cur.execute(" INSERT INTO store VALUES (%s,%s,%s)", %(item,quantity,price))
+    cur.execute(" INSERT INTO store VALUES (%s,%s,%s)",(item,quantity,price))
     conn.commit()
     conn.close()
 
 # insert('Wine Glass',8,10.5)
 
 def view():
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432' ")
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows = cur.fetchall()
@@ -30,22 +30,22 @@ def view():
     return rows
 
 def delete(item):
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432' ")
     cur = conn.cursor()
-    cur.execute("DELETE FROM store WHERE item= ?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432' ")
     cur = conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price= ? WHERE item = ?",(quantity,price,item))
+    cur.execute("UPDATE store SET quantity=%s, price= %s WHERE item = %s",(quantity,price,item))
     conn.commit()
     conn.close()
 
 create_table()
-insert("Apple",10,15)
-#update(11,6,"Wine Glass")
-# delete("Wine Glass")
-#print(view())
+insert("Orange",9,14)
+update(11,6,"Apple")
+delete("Orange")
+print(view())
 #insert('Wine Glass',10,5)
