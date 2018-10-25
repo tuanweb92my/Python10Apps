dedff33b40ee9cb5d799a26501648f3435b20702
nguyenanhtuan92my@gmail.com <nguyenanhtuan92my@gmail.com>
2018-10-25 11:34:36 +0700

    App9c

--- App9/app9.py
+++ App9/app9.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/height_collector'
+#local database on 127.0.0.1
+#app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/height_collector'
+
+#database on heroku :
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://rkrdpytcoxivht:32183447cf82c434e8c6c69583c235f30fcfdc66724788e13d5f8ac45a7d46af@ec2-184-73-222-192.compute-1.amazonaws.com:5432/dcvrq4ouh7j9f9?sslmode=require'
+
 db = SQLAlchemy(app)
 
 class Data(db.Model):
@@ -31,7 +37,7 @@ def success():
         email = request.form["email_name"]
         height = request.form["height_name"]
         print(email,height)
-        send_email(email,height)
+        #send_email(email,height)
         #print(height)
         #print(request.form)
         #print(db.session.query(Data).filter(Data.email_==email))
@@ -41,6 +47,11 @@ def success():
             data=Data(email,height)
             db.session.add(data)
             db.session.commit()
+            average_height = db.session.query(func.avg(Data.height_)).scalar()
+            average_height=round(average_height,1)
+            count = db.session.query(Data.height_).count()
+            send_email(email,height,average_height,count)
+            print(average_height)
             return render_template("success.html")
     return render_template("index.html",
      text="Seems like we've got something from that email address already ")
--- App9/send_email.py
+++ App9/send_email.py
@@ -2,13 +2,13 @@ from email.mime.text import MIMEText
 
 import smtplib
 
-def send_email(email,height):
+def send_email(email,height,average_height,count):
         from_email="tuannguyenanh_lido1995@yahoo.com.vn"
         from_password="12345678@T"
         to_email="nguyenanhtuan92my@gmail.com"
         debuglevel = True
         subject="Height data"
-        message= " Hey there, your height is <strong>%s<strong>. " %height
+        message= " Hey there, your height is <strong>%s</strong>. <br> Average height of all is <strong>%s</strong> and that is calculated out of <strong>%s</strong> people. <br> Thanks! " %(height,average_height,count)
 
         msg = MIMEText(message,'html')
         msg['Subject'] = subject
